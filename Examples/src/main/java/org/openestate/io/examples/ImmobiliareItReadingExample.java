efb2bf7cca409036484ae896a2a5b43688c3fbb9
Andreas Rudolph <andy@openindex.de>
2015-02-24 22:46:15 +0100

    improved example for reading Immobiliare-XML

--- Examples/src/main/java/org/openestate/io/examples/ImmobiliareItReadingExample.java
+++ Examples/src/main/java/org/openestate/io/examples/ImmobiliareItReadingExample.java
@@ -18,24 +18,33 @@ package org.openestate.io.examples;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
 import org.openestate.io.immobiliare_it.ImmobiliareItDocument;
 import org.openestate.io.immobiliare_it.ImmobiliareItUtils;
 import org.openestate.io.immobiliare_it.xml.Feed;
 import org.openestate.io.immobiliare_it.xml.Feed.Properties.Property;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
 /**
- * Example for XML reading.
+ * Example for reading XML files for
+ * <a href="http://immobiliare.it">immobiliare.it</a>.
  * <p>
- * This example illustrates how to read an Trovit document from a file.
+ * This example illustrates how to read XML files for
+ * <a href="http://immobiliare.it">immobiliare.it</a>.
  *
  * @author Andreas Rudolph
  */
 public class ImmobiliareItReadingExample
 {
+  private final static Logger LOGGER = LoggerFactory.getLogger( ImmobiliareItReadingExample.class );
+  private final static String PACKAGE = "/org/openestate/io/examples";
+
   /**
    * Start the example application.
    *
@@ -44,32 +53,50 @@ public class ImmobiliareItReadingExample
    */
   public static void main( String[] args )
   {
+    // init logging
+    PropertyConfigurator.configure(
+      ImmobiliareItReadingExample.class.getResource( PACKAGE + "/log4j.properties" ) );
+
+    // read example files, if no files were specified as command line arguments
     if (args.length<1)
-    {
-      System.out.println( "Please provide at least one Trovit file as argument!" );
-      System.exit( 1 );
-    }
-    for (String arg : args)
     {
       try
       {
-        read( new File( arg ) );
+        read( ImmobiliareItReadingExample.class.getResourceAsStream( PACKAGE + "/immobiliare_it.xml" ) );
       }
       catch (Exception ex)
       {
-        System.err.println( "The provided file is invalid!" );
-        ex.printStackTrace( System.err );
+        LOGGER.error( "Can't read example file!" );
+        LOGGER.error( "> " + ex.getLocalizedMessage(), ex );
         System.exit( 2 );
+      }
+    }
+
+    // read files, that were specified as command line arguments
+    else
+    {
+      for (String arg : args)
+      {
+        try
+        {
+          read( new File( arg ) );
+        }
+        catch (Exception ex)
+        {
+          LOGGER.error( "Can't read file '" + arg + "'!" );
+          LOGGER.error( "> " + ex.getLocalizedMessage(), ex );
+          System.exit( 2 );
+        }
       }
     }
   }
 
   /**
-   * Read an Trovit file into a {@link TransferDocument} and prints some of
-   * their content to console.
+   * Read a {@link File} into an {@link ImmobiliareItDocument} and print some
+   * of its content to console.
    *
    * @param xmlFile
-   * the XML file to read
+   * the file to read
    *
    * @throws SAXException
    * if the file is not readable by the XML parser
@@ -85,26 +112,58 @@ public class ImmobiliareItReadingExample
    */
   protected static void read( File xmlFile ) throws SAXException, IOException, ParserConfigurationException, JAXBException
   {
-    System.out.println( "process file: " + xmlFile.getAbsolutePath() );
+    LOGGER.info( "process file: " + xmlFile.getAbsolutePath() );
     if (!xmlFile.isFile())
     {
-      System.out.println( "> The provided file is invalid!" );
+      LOGGER.warn( "> provided file is invalid" );
       return;
     }
     ImmobiliareItDocument doc = ImmobiliareItUtils.createDocument( xmlFile );
     if (doc==null)
     {
-      System.out.println( "> provided XML is not supported" );
+      LOGGER.warn( "> provided XML is not supported" );
     }
     else
     {
-      System.out.println( "> is transfer XML" );
-      read( doc );
+      printToConsole( doc );
     }
   }
 
   /**
-   * Print the content of a {@link ImmobiliareItDocument} to console.
+   * Read a {@link InputStream} into an {@link ImmobiliareItDocument} and print
+   * some of its content to console.
+   *
+   * @param xmlInputStream
+   * the input stream to read
+   *
+   * @throws SAXException
+   * if the file is not readable by the XML parser
+   *
+   * @throws IOException
+   * if the file is not readable
+   *
+   * @throws ParserConfigurationException
+   * if the XML parser is improperly configured
+   *
+   * @throws JAXBException
+   * if XML conversion into Java objects failed
+   */
+  protected static void read( InputStream xmlInputStream ) throws SAXException, IOException, ParserConfigurationException, JAXBException
+  {
+    LOGGER.info( "process example file" );
+    ImmobiliareItDocument doc = ImmobiliareItUtils.createDocument( xmlInputStream );
+    if (doc==null)
+    {
+      LOGGER.warn( "> provided XML is not supported" );
+    }
+    else
+    {
+      printToConsole( doc );
+    }
+  }
+
+  /**
+   * Print some content of an {@link ImmobiliareItDocument} to console.
    *
    * @param doc
    * the document to process
@@ -112,7 +171,7 @@ public class ImmobiliareItReadingExample
    * @throws JAXBException
    * if XML conversion into Java objects failed
    */
-  protected static void read( ImmobiliareItDocument doc ) throws JAXBException
+  protected static void printToConsole( ImmobiliareItDocument doc ) throws JAXBException
   {
     Feed feed = doc.toObject();
 
@@ -131,8 +190,8 @@ public class ImmobiliareItReadingExample
         if (objectInfo==null) objectInfo = "???";
 
         // print object informations to console
-        System.out.println( ">> property "
-          + "'" + objectNr + "' for sale: " + objectInfo );
+        LOGGER.info( "> found object "
+          + "'" + objectNr + "': " + objectInfo );
       }
     }
   }
