03d836798a3577eb8a9795c27a67155e63062b95
Andreas Rudolph <andy@openindex.de>
2016-02-22 14:23:07 +0100

    fixed handling of positive decimal values in OpenImmo

--- OpenImmo/src/main/java/org/openestate/io/openimmo/OpenImmoUtils.java
+++ OpenImmo/src/main/java/org/openestate/io/openimmo/OpenImmoUtils.java
@@ -399,13 +399,15 @@ public class OpenImmoUtils
 
   public static String printPositiveDecimal( BigDecimal value )
   {
-    if (value==null || value.compareTo( BigDecimal.ZERO )<1)
+    // OpenImmo specifies positive decimal values including 0
+    if (value==null || value.compareTo( BigDecimal.ZERO )<0)
       throw new IllegalArgumentException( "Can't print positive decimal value!" );
     return printDecimal( value.setScale( 2, BigDecimal.ROUND_HALF_UP ) );
   }
 
   public static String printPositiveInteger( BigInteger value )
   {
+    // OpenImmo specifies positive integer values excluding 0
     if (value==null || value.compareTo( BigInteger.ZERO )<1)
       throw new IllegalArgumentException( "Can't print positive integer value!" );
     else
