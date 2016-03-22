3d3fd6e2153b9d9522d00a3fd2cdd788bcef4853
Andreas Rudolph <andy@openindex.de>
2016-03-23 00:25:28 +0100

    minor changes to XmlValidationHandler

--- Core/src/main/java/org/openestate/io/core/XmlValidationHandler.java
+++ Core/src/main/java/org/openestate/io/core/XmlValidationHandler.java
@@ -34,27 +34,38 @@ public class XmlValidationHandler implements ValidationEventHandler
   /**
    * Receive notification of a validation warning or error.
    *
+   * <p>
+   *
    * The ValidationEvent will have a
    * {@link javax.xml.bind.ValidationEventLocator} embedded in it that
    * indicates where the error or warning occurred.
    *
    * <p>
+   *
    * If an unchecked runtime exception is thrown from this method, the JAXB
    * provider will treat it as if the method returned false and interrupt
    * the current unmarshal, validate, or marshal operation.
    *
-   * @param event the encapsulated validation event information.  It is a
-   * provider error if this parameter is null.
-   * @return true if the JAXB Provider should attempt to continue the current
-   *         unmarshal, validate, or marshal operation after handling this
-   *         warning/error, false if the provider should terminate the current
-   *         operation with the appropriate <tt>UnmarshalException</tt>,
-   *         <tt>ValidationException</tt>, or <tt>MarshalException</tt>.
-   * @throws IllegalArgumentException if the event object is null.
+   * @param event
+   * the encapsulated validation event information. It is a provider error if
+   * this parameter is null.
+   *
+   * @return
+   * true if the JAXB Provider should attempt to continue the current unmarshal,
+   * validate, or marshal operation after handling this warning/error, false if
+   * the provider should terminate the current operation with the appropriate
+   * <tt>UnmarshalException</tt>, <tt>ValidationException</tt>, or
+   * <tt>MarshalException</tt>.
+   *
+   * @throws IllegalArgumentException
+   * if the event object is null.
    */
   @Override
   public boolean handleEvent( ValidationEvent event )
   {
+    if (event==null)
+      throw new IllegalArgumentException( "No validation event was provided!" );
+
     int line = -1;
     int col = -1;
     if (event.getLocator()!=null)
