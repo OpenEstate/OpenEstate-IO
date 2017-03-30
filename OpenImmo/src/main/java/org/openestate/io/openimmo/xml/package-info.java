4cc903582247a2280a172dedf77245e26ae0bffa
Jochen Niebuhr <jniebuhr@cfire.de>
2017-03-30 18:48:08 +0200

    Resolve some namespace issues
    When specifying a namespace, JAXB couldn't parse it.
    You would then have to add a package-info.java to resolve that issue.
    Which leads to another issue of parsing failures without a namespace.

---
+++ OpenImmo/src/main/java/org/openestate/io/openimmo/xml/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(namespace = "http://www.openimmo.de", elementFormDefault = XmlNsForm.UNQUALIFIED)
+package org.openestate.io.openimmo.xml;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
